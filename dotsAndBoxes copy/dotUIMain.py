e976ff2d2e59e253f46bda1069fc8a8d55d683d3
loganrivera <loganrivera@icloud.com>
2020-05-06 22:07:36 -0500

    Finished Basic elements of UI design

--- dotsAndBoxes copy/dotUIMain.py
+++ dotsAndBoxes copy/dotUIMain.py
@@ -4,60 +4,59 @@ import pygame
 
 class game:
     blockSize = 60
-    backgroundColor = (255, 255, 255)
-    selectionColor = (255, 255, 255)
+    backgroundColor = (255, 226, 188)
+    dotColor = (88, 180, 174)
 
 
-    def __init__(self, whiteSquareColor=(175, 29, 59), blackSquareColor=(77,70,70)):
-        self.whiteSquareColor = whiteSquareColor
-        self.blackSquareColor = blackSquareColor
+    def __init__(self):
+
 
         self.numDots = 20
-        self.screenMuliplier = 30
+        self.screenMuliplier = 40
         self.size = self.numDots * self.screenMuliplier
-        self.environment = env(self.size, self.size, 'chess')
-        self.screenBuffer = int(self.size / 8)
+        self.environment = env(self.size, self.size, 'DotsAndBoxes')
+        self.screenBuffer = int(self.size / 12)
         self.dotSize = 8
+        self.dotBuffer = self.dotSize * 4
 
         self.done = False
         self.positionSelected = None
         self.playerTurn = 0
+        self.getDotsForLocation((0, 19))
 
 
     def updateGameScreen(self):
         self.environment.resetBackGround(allBackGround=True)
         self.drawBoard()
         self.environment.renderChildren()
+        self.getDotsForLocation((0, 18))
         pygame.display.flip()
 
 
     def drawBoard(self):
-        # for rowNum, row in enumerate((num)):
-        #     for columnNum, block in enumerate(row):
-        #         blockPostionInView = ((rowNum * self.blockSize), (columnNum * self.blockSize))
-        #         blockRect = pygame.Rect(blockPostionInView, (self.blockSize, self.blockSize))
-        #         if (rowNum + columnNum) % 2 == 0:
-        #             pygame.draw.rect(self.environment.display, self.whiteSquareColor, blockRect)
-        #         elif (rowNum + columnNum) % 2 != 0:
-        #             pygame.draw.rect(self.environment.display, self.blackSquareColor, blockRect)
-
-
         for dotRowNum in range(self.numDots):
             for dotColumnNum in range(self.numDots):
-                spaceBetweenDots = int((self.size - (self.screenBuffer * 2)) / self.numDots)
+                spaceBetweenDots = int((self.size - (self.screenBuffer * 2)) / (self.numDots-1))
                 dotPostionX = self.screenBuffer + spaceBetweenDots * dotRowNum
                 dotPostionY = self.screenBuffer + spaceBetweenDots * dotColumnNum
-                print(dotPostionX, dotPostionY)
-                pygame.draw.circle(self.environment.display, self.environment.RED, (dotPostionX, dotPostionY), self.dotSize)
+                pygame.draw.circle(self.environment.display, self.dotColor, (dotPostionX, dotPostionY), self.dotSize)
 
 
+    def getDotsForLocation(self, location):
+        spaceBetweenDots = int((self.size - (self.screenBuffer * 2)) / (self.numDots - 1))
+        topLeft = ((location[0] * spaceBetweenDots + self.screenBuffer), location[1] * spaceBetweenDots + self.screenBuffer)
+        topRight = (((location[0] + 1) * spaceBetweenDots + self.screenBuffer), location[1] * spaceBetweenDots + self.screenBuffer)
+        bottomLeft = (((location[0]) * spaceBetweenDots + self.screenBuffer), (location[1] + 1) * spaceBetweenDots + self.screenBuffer)
+        bottomRight = (((location[0] + 1) * spaceBetweenDots + self.screenBuffer), (location[1] + 1) * spaceBetweenDots + self.screenBuffer)
+        dotLocations = {"topLeft": topLeft, "topRight": topRight, "bottomLeft": bottomLeft, "bottomRight": bottomRight}
+
+        return dotLocations
 
 
+    def drawLine(self, location, numToFactorize):
+        dotLocations = self.getDotsForLocation(location)
 
-
-
-
-
+        pass
 
     def run(self):
         """
