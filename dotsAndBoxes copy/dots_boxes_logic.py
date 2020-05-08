164cfc0852a6151c798bdf8fbd0d71d24ed2cf6a
loganrivera <loganrivera@icloud.com>
2020-05-07 20:25:27 -0500

    Bug fixed scores and fixed double repeat moves and added player turn indicator

--- dotsAndBoxes copy/dots_boxes_logic.py
+++ dotsAndBoxes copy/dots_boxes_logic.py
@@ -44,7 +44,8 @@ def board_edit(board, position, player, row_box, col_box):
                     board[row_box - X][col_box - Y] *= posit_list[posit_dict[position]] ** player
                 elif a == extra_list[posit_dict[position]]:
                     board[row_box - X][col_box - Y] *= posit_list[(posit_dict[position]+2) % 4] ** player
-                if board[row_box - X][col_box - Y] % 210 == 0:
+                if board[row_box - X][col_box - Y] % 210 == 0 and (not board[row_box - X][col_box - Y] % 11 == 0) and\
+                        (not board[row_box - X][col_box - Y] % 13 == 0):
                     board[row_box - X][col_box - Y] *= (2*player + 9)
         except:
             continue
